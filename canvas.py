a9f33ba29d30b3b42dc86b21b7d152e64f398b13
Lauren <ls_garwood@yahoo.co.uk>
2022-02-11 16:34:43 +0000

    adding mouse pointer function

--- canvas.py
+++ canvas.py
@@ -7,3 +7,19 @@ window = tkinter.Tk()
 canvas = tkinter.Canvas(window, width=75, height=500, bg="white")
 canvas.pack()
 
+lastX, lastY = 0,0
+colour = "black"
+
+def store_position(event):
+    global lastX, lastY
+    lastX = event.x
+    lastY = event.y
+
+def on_click(event):
+    store_position(event)
+
+def on_drag(event):
+    canvas.create_line(lastX, lastY, event.x, event.y, fill = colour, width = 3)
+    store_position(event)
+
+
