7f02e399b473cac79bbc57ca2ace258e704f05bb
Lauren <ls_garwood@yahoo.co.uk>
2022-02-11 17:19:00 +0000

    syntax fixes

--- canvas.py
+++ canvas.py
@@ -4,7 +4,7 @@ print("To draw, hold down the left mouse button, and move your mouse")
 print("To chnage your brush colour, click on one of the colours")
 
 window = tkinter.Tk()
-canvas = tkinter.Canvas(window, width=75, height=500, bg="white")
+canvas = tkinter.Canvas(window, width=750, height=500, bg="white")
 canvas.pack()
 
 lastX, lastY = 0,0
